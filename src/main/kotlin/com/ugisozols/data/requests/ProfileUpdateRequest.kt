1fdb8345b92afac2f4d0b057ecbd2820d81ad44a
Ozzy <Ozols.ugiss@gmail.com>
2021-10-27 23:54:21 +0300

    profile update request data class added

---
+++ src/main/kotlin/com/ugisozols/data/requests/ProfileUpdateRequest.kt
@@ -0,0 +1,23 @@
+package com.ugisozols.data.requests
+
+import com.ugisozols.data.models.*
+
+data class ProfileUpdateRequest(
+    val email : String,
+    val password : String,
+    val name : String,
+    val lastName : String,
+    val education : Education? = null,
+    val profession : String?,
+    val experience : Int?,
+    val profileImageUrl : String,
+    val bio : String?,
+    val instagramUrl : String?,
+    val linkedInUrl : String?,
+    val githubUrl : String?,
+    val skills : List<Skills> = listOf(),
+    val currentJobState : CurrentJobState? = null,
+    val profileUpdateDate : Long?,
+    val keywords : List<Keywords> = listOf(),
+    val category : Categories?,
+)
