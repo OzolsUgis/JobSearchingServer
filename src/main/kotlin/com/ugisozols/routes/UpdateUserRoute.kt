f9525b0e68333a4aaa181913a84cb910539921a5
Ozzy <Ozols.ugiss@gmail.com>
2021-10-27 23:58:14 +0300

    update users profile route added

---
+++ src/main/kotlin/com/ugisozols/routes/UpdateUserRoute.kt
@@ -0,0 +1,49 @@
+package com.ugisozols.routes
+
+import com.ugisozols.data.models.User
+import com.ugisozols.data.requests.ProfileUpdateRequest
+import com.ugisozols.data.responses.MainApiResponse
+import com.ugisozols.service.UserService
+import com.ugisozols.util.Constants
+import com.ugisozols.util.Constants.ERROR_ACCESS_DENIED
+import com.ugisozols.util.QueryParameters
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import sun.applet.Main
+
+fun Route.updateUser(
+    userService: UserService
+){
+    authenticate {
+        post("api/user/profile/update") {
+            val request = call.receiveOrNull<ProfileUpdateRequest>()?: kotlin.run {
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+            val callerUserId = call.parameters[QueryParameters.QUERY_PARAM_USER_ID]
+            if(!userService.checkIfUsersIdIsEqualToProfileId(callerUserId ?: "",call.userId?: "")){
+                call.respond(
+                    HttpStatusCode.Unauthorized,
+                    MainApiResponse<Unit>(
+                        false,
+                        ERROR_ACCESS_DENIED
+                    )
+                )
+                return@post
+            }
+            if(userService.updateUser(call.userId?: "", request)) {
+                call.respond(
+                    HttpStatusCode.OK,
+                    MainApiResponse<Unit>(
+                        true,
+                        "Account successfully updated"
+                    )
+                )
+            }
+        }
+    }
+}
